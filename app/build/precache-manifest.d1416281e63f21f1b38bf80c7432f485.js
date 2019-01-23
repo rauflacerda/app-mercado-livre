c8a1d726e4b958c4ad84030b90d23d6cfcd6339b
Rauf Lacerda <rauf.lacerda@docsystemcorp.com>
2019-01-23 16:15:13 -0200

    Inclusão dos arquivos do build

---
+++ app/build/precache-manifest.d1416281e63f21f1b38bf80c7432f485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f58cad2e0f699254a7fb",
+    "url": "/static/js/main.f58cad2e.chunk.js"
+  },
+  {
+    "revision": "706eb59319b5db6a3bd9",
+    "url": "/static/js/1.706eb593.chunk.js"
+  },
+  {
+    "revision": "f58cad2e0f699254a7fb",
+    "url": "/static/css/main.9344d8d7.chunk.css"
+  },
+  {
+    "revision": "a6493022f63228097de26377f508a6be",
+    "url": "/index.html"
+  }
+];
